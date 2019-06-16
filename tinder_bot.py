f2004980e86a838084898bd98bcf30beb926e325
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-06-15 20:13:58 -0400

    bora krai

--- tinder_bot.py
+++ tinder_bot.py
@@ -188,7 +188,7 @@ class TinderBot:
 		else:
 			response = r.json()
 			new_token = getToken(str(config.lucas_phone_number), str(response['sms_code']), str(log_code))
-			self.update_token(str(new_token))
+			self.update_token(new_token)
 			time.sleep(5)
 			try:
 				tr = tinder_api.get_self()
@@ -198,7 +198,7 @@ class TinderBot:
 					r = requests.get(config.aws_host + '/auth_code')
 					response = r.json()
 					new_token = getToken(str(config.lucas_phone_number), str(response['sms_code']), str(log_code))
-					self.update_token(str(new_token))
+					self.update_token(new_token)
 					time.sleep(5)
 					tr = tinder_api.get_self()
 				tell_lucas = requests.get(config.aws_host + '/success')
