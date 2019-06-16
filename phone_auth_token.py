93209dde2b206c3612fc439de6db1837f8711ca8
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-06-15 20:45:56 -0400

    fixed phone auth

--- phone_auth_token.py
+++ phone_auth_token.py
@@ -35,7 +35,7 @@ def getToken(number, code, req_code):
     else:
         return token_response["data"]["api_token"]
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     phone_number = input("Please enter your phone number under the international format (country code + number)")
     log_code = sendCode(phone_number)
     sms_code = input("Please enter the code you've received by sms")
